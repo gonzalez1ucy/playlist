db70740eaf8d7ff2147ea198eb03c0518dc1ffe1
Luzmarie Gonzalez <glucy822@gmail.com>
2019-02-25 03:50:53 +0000

    final

--- js/script.js
+++ js/script.js
@@ -5,40 +5,52 @@ var songs = ["Cinema", "Miedo","Wokeuplikethis*","Everyone Nose (all the girls s
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 var link=["https://www.youtube.com/watch?v=bx1xdUJcEoA","https://www.youtube.com/watch?v=TcvnS69SxKs","https://www.youtube.com/watch?v=REmZhFKmOmo","https://www.youtube.com/watch?v=N2wqmGu1EY8","https://www.youtube.com/watch?v=6AHYl3dXeN4", "https://www.youtube.com/watch?v=c15QgtYqknA"];
-var artist=["Kero Kero Bonito","Beach Goons","PlayboiCarti(ft lil uzi vert", "N.E.R.D","Kevin Abstract","Outkast"];
+var artists=["Kero Kero Bonito","Beach Goons","PlayboiCarti(ft lil uzi vert)", "N.E.R.D","Kevin Abstract","Outkast"];
 var image_links=["https://consequenceofsound.files.wordpress.com/2018/10/kero-kero-bonito-time-n-place-album-art.jpg?quality=80","https://images-na.ssl-images-amazon.com/images/I/51yeAVJVgPL._SS500.jpg","https://i.redd.it/fu2wul4d18sy.jpg","https://images-na.ssl-images-amazon.com/images/I/51Dotze0lvL._SS500.jpg", "https://images.genius.com/b5167f25c3f5b224b73ea09a611add77.1000x1000x1.jpg","https://images-na.ssl-images-amazon.com/images/I/61T3AEFkPPL._SS500.jpg"];
-var songlengths =[120,117,260,205,177];
+var song_lengths =[120,117,260,205,177];
 
 function displaySongInfo(){
     songs.forEach(function(song) {
          $("#songs").append(" <p> " + song +  "</p>");  
     });
     
-    link.forEach(function(links){
-         $("#link").append(" <p> " + links +  "</p> ");    
+    link.forEach(function(videolink){
+         $("#links").append( "<a href=" + videolink + ">" + "Preview" + "</a>");    
     });
     
     image_links.forEach(function(image){
-        $("#image").append(" <p> " + image +  " </p> ");     
+        $("#images").append( "<img src=" + image +  ">");     
     });
     
-    songlengths.forEach(function(length){
-        $("#length").append(" <p> " + length +  " </p> ");     
+    song_lengths.forEach(function(length){
+        $("#lengths").append(" <p> " + length +  " </p> ");     
+    });
+    artists.forEach(function(artist) {
+         $("#artists").append(" <p> " + artist +  "</p>");  
     });
 }
 
 function emptySongInfo(){
     $("#songs").empty();
-    // Use jQuery to empty all of the remaining divs
-
-
+    $("#images").empty();
+    $("#artists").empty();
+    $("#lengths").empty();
+    $("#links").empty();
 }
 
 
 function addSongInfo(){
     // BELOW write the code to add new items to each of the arrays.
-
-
+    var newSong = $("#song").val();
+         songs.push(newSong);
+    var newArtist = $("#artisit").val();
+        artists.push(newArtist);
+    var newLength = $("#length").val();
+       song_lengths.push(newLength);
+    var newImage = $("#image").val();
+       image_links.push(newImage);
+    var newLink = $("#link").val();
+       link.push(newLink);
 }
 
 $("#add").click(function() {
